ef9db0889c5ccfbbed6179c45497299f8169ccc7
Y5 <48315261+Y5-asas@users.noreply.github.com>
2024-11-29 01:54:46 +0800

    Update train_sentiment.py

--- train_sentiment.py
+++ train_sentiment.py
@@ -8,6 +8,7 @@ from model_senti.Senti_DataLoader import built_curpus, SentiDataset
 from model_senti.CNN_senti import CNNnet # Assuming CNN_senti is in cnn_senti_model.py
 from model_senti.MLP_senti import MLPModel  # Assuming MLPModel is in mlp_senti_model.py
 from model_senti.SVM_senti import *
+from torch.optim.lr_scheduler import OneCycleLR
 
 # Argument Parser
 def parse_args():
@@ -19,7 +20,16 @@ def parse_args():
     parser.add_argument('--max_length', type=int, default=1000, help="Maximum sequence length")
     parser.add_argument('--embedding_dim', type=int, default=512, help="Embedding dimension")
     parser.add_argument('--num_classes', type=int, default=3, help="Number of output classes")
-    parser.add_argument('--model', type=str, default="MLP", choices=["MLP", "CNN","SVM"], help="Model type")
+    parser.add_argument('--model', type=str, default="MLP", choices=["MLP", "CNN", "SVM"], help="Model type")
+
+    parser.add_argument('--mlp_lr', type=float, default=0.0005, help="MLP Learning rate")
+    parser.add_argument('--cnn_lr', type=float, default=0.001, help="CNN Learning rate")
+    parser.add_argument('--mlp_batch_size', type=int, default=32, help="MLP batch size")
+    parser.add_argument('--cnn_batch_size', type=int, default=64, help="CNN batch size")
+    parser.add_argument('--mlp_epochs', type=int, default=30, help="MLP Number of training epochs")
+    parser.add_argument('--cnn_epochs', type=int, default=30, help="CNN Number of training epochs")
+    parser.add_argument('--mlp_embedding_dim', type=int, default=256, help="MLP Embedding dimension")
+
     return parser.parse_args()
 
 
@@ -35,21 +45,32 @@ def train():
     senti_word_2_index = built_curpus(train_data['text'])
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    # Initialize model
+    if args.model == "MLP":
+        model = MLPModel(len(senti_word_2_index), args.embedding_dim, args.num_classes, args.dropout)
+        args.epochs = args.mlp_epochs
+        args.batch_size = args.mlp_batch_size
+        args.lr = args.mlp_lr
+        args.embedding_dim = args.mlp_embedding_dim
+    elif args.model == "CNN":
+        model = CNNnet(len(senti_word_2_index), args.embedding_dim, args.num_classes, args.dropout)
+        args.epochs = args.cnn_epochs
+        args.batch_size = args.cnn_batch_size
+        args.lr = args.cnn_lr
+
     # Create DataLoaders
     train_dataset = SentiDataset(train_data, senti_word_2_index, args.max_length)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
     val_dataset = SentiDataset(val_data, senti_word_2_index, args.max_length)
     val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
 
-    # Initialize model
-    if args.model == "MLP":
-        model = MLPModel(len(senti_word_2_index), args.embedding_dim, args.num_classes, args.dropout)
-    elif args.model == "CNN":
-        model = CNNnet(len(senti_word_2_index), args.embedding_dim, args.num_classes, args.dropout)
     model = model.to(device)
 
     optimizer = AdamW(model.parameters(), lr=args.lr)
-    loss_fn = nn.CrossEntropyLoss()
+    scheduler = OneCycleLR(optimizer, args.lr, total_steps=args.epochs * len(train_loader), pct_start=0.25,
+                           anneal_strategy='cos')
+    loss_fn = nn.CrossEntropyLoss(reduction = 'mean')
+
 
     # Training loop
     for epoch in range(args.epochs):
@@ -64,6 +85,7 @@ def train():
             loss = loss_fn(outputs, labels)
             loss.backward()
             optimizer.step()
+            scheduler.step()
 
             total_loss += loss.item()
             correct += (labels == outputs.argmax(dim=1)).sum().item()
@@ -88,10 +110,14 @@ def train():
 
         print(f"Epoch {epoch + 1}, Val Loss: {total_loss / len(val_loader):.4f}, Val Acc: {correct / total:.4f}")
     return model
+
+
 def test(model):
     test_file_path = 'nusax-main/datasets/sentiment/indonesian/test.csv'
+    train_file_path = 'nusax-main/datasets/sentiment/indonesian/train.csv'
+    train_data = pd.read_csv(train_file_path, usecols=lambda col: col != 'Unnamed: 0')
     test_data = pd.read_csv(test_file_path, usecols=lambda col: col != 'Unnamed: 0')
-    senti_word_2_index = built_curpus(test_data['text'])
+    senti_word_2_index = built_curpus(train_data['text'])
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     test_dataset = SentiDataset(test_data, senti_word_2_index, max_length=1000)
@@ -117,14 +143,13 @@ def test(model):
     print(f"Model Structure: {model}")
 
 
-
 def train_svm(train_file, valid_file, test_file):
     train_data = pd.read_csv(train_file)
     valid_data = pd.read_csv(valid_file)
     test_data = pd.read_csv(test_file)
 
-    X_train, y_train, X_valid, y_valid, X_test, y_test = Data_encoder(train_data, valid_data, test_data)
     model = Senti_SVM_model()
+    X_train, y_train, X_valid, y_valid, X_test, y_test = Data_encoder(train_data, valid_data, test_data)
     model.fit(X_train, y_train, X_valid, y_valid)
     model.evaluate(X_test, y_test)
 
